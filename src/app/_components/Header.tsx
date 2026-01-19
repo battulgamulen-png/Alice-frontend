5fd80054d6adf74192d1defb9f53176cd818864f
mulen <mulen@mulens-MacBook-Air.local>
2026-01-19 20:55:58 +0800

    first repo

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header({ name }: { name: string }) {
+  return <div className="text-black bg-white h-10  ">{name}</div>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="w-full ">
+        <Header name={"Mulen"} />
+      </div>
+      <div></div>
+    </div>
+  );
+}
